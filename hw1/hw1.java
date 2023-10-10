231766a156c7a9092e9f0af72d2e09d153093378
artchann <artchann@gmail.com>
2023-10-09 22:46:23 -0700

    Tri finished, testers present, no edge cases tested

--- hw1/hw1.java
+++ hw1/hw1.java
@@ -18,14 +18,50 @@ public class hw1{
 		class Triangle extends Shape{
 			private int base;
 			private int height;
-			
 			public Triangle(String name) {
 				super(name);
 			}
 			
+			//tester functions
+			void setHeight(int height) {
+				this.height = height;
+			}
+			void setBase(int base) {
+				this.base = base;
+			}
+			//
+			
+			@Override
+			double area() {
+				double x = Double.valueOf(base);
+				double y = Double.valueOf(height);
+				return x*y/2;
+			}
+			
+			@Override
+			void print() {
+				System.out.println(name + "(" + base + ", " + height + ") : " + area() );
+			}
+			
+			@Override
+			void draw() {
+				System.out.println("   *   ");
+				System.out.println("  * *  ");
+				System.out.println(" *   * ");
+				System.out.println("*******");
+			}
 			
 		}
-		Triangle x = new Triangle("S");
+		Triangle x = new Triangle("First triangle");
+		x.setBase(5);
+		x.setHeight(5);
+		x.print();
+		x.draw();
+		Triangle y = new Triangle("Second triangle");
+		y.setBase(4);
+		y.setHeight(4);
+		y.print();
+		y.draw();
 	}
 }
 
