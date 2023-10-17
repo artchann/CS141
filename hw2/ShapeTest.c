6503488974451434c983f958118554f7d4c02b0f
artchann <artchann@gmail.com>
2023-10-16 19:40:39 -0700

    Applied partial functionality to inherited triangle

--- hw2/ShapeTest.c
+++ hw2/ShapeTest.c
@@ -14,6 +14,10 @@ typedef union{
     char_method_type charMethod;
 }VTableUnit;
 
+#define GET_NAME_INDEX 0
+#define GET_AREA_INDEX 1
+#define PRINT_INDEX 2
+#define DRAW_INDEX 3
 
 struct Shape{
     VTableUnit* vPtr;
@@ -39,14 +43,44 @@ Shape* Shape_Constructor(Shape* _this, char* name){
 
 struct Triangle {
     VTableUnit* vPtr;
-    double base;
-    double height;
+    char* name;
+    int base;
+    int height;
 };
+//triangle functions
+
+double getArea(Triangle* _this){
+    double area = (double)_this->base*(double)_this->height / 2;
+    return area;
+}
+
+VTableUnit Triangle_VTable [] = {
+    {.charMethod=(char_method_type)getName},
+    {.doubleMethod=(double_method_type)getArea}
+};
+
+Triangle* Triangle_Constructor(Triangle* _this, char* name, int base, int height){
+    _this->vPtr = Triangle_VTable;
+    _this->name = name;
+    _this->base = base;
+    _this->height = height;
+    return _this;
+}
+
+//
 
 int main(){
     char name[] = "Shape";
     Shape* a = Shape_Constructor((Shape*)malloc(sizeof(Shape)), name);
-    char* nameFinal = a->vPtr[0].charMethod(a);
+    char* nameFinal = a->vPtr[GET_NAME_INDEX].charMethod(a);
     printf("%s\n", nameFinal);
+
+    char triName[] = "Triangle";
+    int base = 5;
+    int height = 5;
+    Triangle* tri = Triangle_Constructor((Triangle*)malloc(sizeof(Shape)), triName, base, height);
+
+    printf("%f\n", tri->vPtr[GET_AREA_INDEX].doubleMethod(tri));
+    printf("%s\n", tri->vPtr[GET_NAME_INDEX].charMethod((Shape*)tri));
 }
 
