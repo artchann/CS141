90013683866c8ffbe14892dddba5ef8de4b41b2b
artchann <artchann@gmail.com>
2023-11-15 05:42:15 -0800

    Remove, substitution, and subset added; substitution currently experiencing errors, refer to comment line

--- hw6/main.pl
+++ hw6/main.pl
@@ -16,7 +16,19 @@ my_nth([H|T], 0, [H|T]).
 % my_nth([H|T], R, [H2|T2]) :- R < 0 //condition?
 my_nth([_H|T], R, L) :- New is R -1, my_nth(T, NewR, L).
 
+my_remove(_, [], []).
+my_remove(H, [H|T], R) :- my_remove(H, T, R).
+my_remove(X, [H|T], [H|NewTail]) :- my_remove(X, T, NewTail).
 
+
+%Error present, won't return list but performs all substitutions
+my_subst(_F, _R, [], []).
+my_subst(F, R, [H|T], [H|NewTail]) :- my_subst(F, R, T, NewTail).
+my_subst(F, R, [F|T], [R|NewTail]) :- my_subst(F, R, T, NewTail).
+
+my_subset(_P, [], []).
+my_subset(P, [H|T], [H|NewTail]) :- call(P, H), my_subset(P, T, NewTail).
+my_subset(P, [_H|T], NewTail) :- my_subset(P, T, NewTail).
 
 mult_by_two([], []).
 mult_by_two([H|T], [NewH|R]) :-
