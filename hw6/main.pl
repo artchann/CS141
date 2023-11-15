0ba92a1c9c11af5e5be6b07aef2387cae7d82ad9
artchann <artchann@gmail.com>
2023-11-15 06:26:47 -0800

    Added my-merge, basic functionality works

--- hw6/main.pl
+++ hw6/main.pl
@@ -1,20 +1,22 @@
 my_length([],0).
-my_length([H|T],R) :- my_length(T, TLen), R is TLen + 1.
+my_length([_|T],R) :- my_length(T, TLen), R is TLen + 1.
 
 my_member(_A,[]) :- false.
+
 my_member(A, [H|_T]) :- A == H.
 my_member(A, [_H|T]) :- mem(A,T).
 
 my_append([], L, L).
 my_append([H|T], L, [H|NewT]) :- my_append(T,L,NewT).
 
-my_reverse([], []).
-my_reverse([H|T], R) :- my_reverse(T, NextR), myappend(NextR, [H], R). %[H] constructs a new list.. how come this is necessary?
+my_reverse_helper([], R ,R).
+my_reverse_helper([H|T], R, Result) :- my_reverse_helper(T, R, [H|Result]).
+my_reverse(L, R) :- my_reverse_helper(L, R, []).
 
 my_nth([], _R, []).
-my_nth([H|T], 0, [H|T]).
+my_nth([H|T], 1, [H|T]).
 % my_nth([H|T], R, [H2|T2]) :- R < 0 //condition?
-my_nth([_H|T], R, L) :- New is R -1, my_nth(T, NewR, L).
+my_nth([_H|T], R, L) :- NewR is R -1, my_nth(T, NewR, L).
 
 my_remove(_, [], []).
 my_remove(H, [H|T], R) :- my_remove(H, T, R).
@@ -29,6 +31,11 @@ my_subst(F, R, [F|T], [R|NewTail]) :- my_subst(F, R, T, NewTail).
 my_subset(_P, [], []).
 my_subset(P, [H|T], [H|NewTail]) :- call(P, H), my_subset(P, T, NewTail).
 my_subset(P, [_H|T], NewTail) :- my_subset(P, T, NewTail).
+
+my_merge([], L, L).
+my_merge(L, [], L).
+my_merge([H1|T1], [H2|T2], [H1|R]) :- H1 =< H2, my_merge(T1, [H2|T2], R).
+my_merge([H1|T1], [H2|T2], [H2|R]) :- H2 < H1, my_merge([H1|T1], T2, R).
 
 mult_by_two([], []).
 mult_by_two([H|T], [NewH|R]) :-
